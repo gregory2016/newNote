ccfc5a01db3f8f4432976711c21a62a944d145b2
gregory2016 <chunogreg@gmail.com>
2016-08-17 15:01:14 +0100

    updated my program

--- note_content.js
+++ note_content.js
@@ -14,26 +14,48 @@ class NotesApplication {
           text : "this is my turn to shine"
          }) 
 
-create(note){
-         	this.note.push(note);
+create(notes){
+         	this.note.push(notes);
+          noteIndex++;
          }
       listNotes(){
+        {
+          console.log(note)
+        }
       	
       }
       	
 get(note_id) {
+  for(var index = 0; index >= note.length-1; index++){
+    if(noteIndex === note_id){
+        return note.text;
+  }
 	
 }
+}
 search(search_text){
-	
+for(var index = 0; index >= note.length-1; index++){
+    if(note[text] === search_text){
+        return note.text;
+  }
+  
+}	
+
 } 
  
 delete(note_id){
-
+for(var index = 0; index >= note.length-1; index++){
+    if(note[index] === note[note_id]){
+         note[index] = null;
+  }
 	
 }
 edit(note_id, new_content){
-
+for(var index = 0; index >= note.length-1; index++){
+    if(note[index] === note[note_id]){
+         note[text] = new_content;
+  }
+  
 	
 }
 
