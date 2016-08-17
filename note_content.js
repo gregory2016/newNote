036e0255b5f2dcf5fe71ec6bfaac04bdaab2a2c5
gregory2016 <chunogreg@gmail.com>
2016-08-17 13:04:16 +0100

    adding a file

---
+++ note_content.js
@@ -0,0 +1,44 @@
+class Note{
+  constructor(properties);
+     this.author = properties.author;
+     this.note_content = note_content;  
+
+}
+class NotesApplication {
+ 
+     constructor (author, note) {
+         this.author = author;
+         this.note = [];
+         var note = new Note({
+          author : "Gregory",
+          text : "this is my turn to shine"
+         }) 
+
+create(note){
+         	this.note.push(note);
+         }
+      listNotes(){
+      	
+      }
+      	
+get(note_id) {
+	
+}
+search(search_text){
+	
+} 
+ 
+delete(note_id){
+
+	
+}
+edit(note_id, new_content){
+
+	
+}
+
+
+
+
+     }
+ }
